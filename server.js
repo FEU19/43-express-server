95d4c552a7a26a63d5f3d08c0eb39ef174d7d034
David Andersson <lejonmanen@gmail.com>
2020-08-13 15:35:13 +0200

    Exercise 2.1-2

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const port = 1234;  // arbitrary port number
 
 
 app.get('/', (request, response) => {
+    console.log('GET /');
     response.send('The cake is a lie')
 })
 
 app.get('/hello', (req, res) => {
+    console.log('GET /hello');
     res.send('Hello!')
 })
 
